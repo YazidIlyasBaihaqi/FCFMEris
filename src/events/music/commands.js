7a978a423bbe950922eeb22de412e8a140d10231
YazidIlyas <72720176+YazidIlyasBaihaqi@users.noreply.github.com>
2022-10-13 08:37:17 +0700

    Fixed and improve some stuff plz work

--- src/events/music/commands.js
+++ src/events/music/commands.js
@@ -2,54 +2,54 @@ const ytdl = require("ytdl-core");
 const ytSearch = require("yt-search");
 const ytpl = require("ytpl");
 const SQueue = require("../../database/models/server_queueSchema");
-let { Types } = require("mongoose");
+let { Types, connection } = require("mongoose");
 const connectionMap = new Map();
 const Eris = require("eris");
 require("pluris")(Eris);
 const Constants = Eris.Constants;
 const fs = require("fs");
 var path = require("path");
+const ytdlexec = require('youtube-dl-exec');
+const { resolve } = require("path");
 
-async function music(bot, message, args, todo) {
-  const voice_channel = message.member.voiceState;
-  if (connectionMap.get(message.guildID)) {
+//note, tambahin argument di fungsi "interaction" atau kalau bisa ganti message jadi interaction
+async function music(bot, interactionPast, args, todo) {
+  const voice_channel = interactionPast.member.voiceState;
+  if (connectionMap.get(interactionPast.guildID)) {
     if (
-      voice_channel.channelID != connectionMap.get(message.guildID).channelID
+      voice_channel.channelID != connectionMap.get(interactionPast.guildID).channelID
     ) {
-      return bot.createMessage(
-        message.channel.id,
+      return interactionPast.createMessage(
         "Please join the current playing voice channel"
       );
     }
   }
 
   if (!voice_channel.channelID) {
-    return bot.createMessage(
-      message.channel.id,
+    return interactionPast.createMessage(
       "Please join the voice channel to use this command"
     );
   }
 
   let database_queue = await SQueue.findOne({
-    guildID: message.guildID,
+    guildID: interactionPast.guildID,
   });
 
-  console.log("masuk function");
   switch (todo) {
     case "play":
       console.log("masuk");
       let song = {};
       //note fix play
-      if (ytpl.validateID(args[0])) {
-        const playlist = await ytpl(args[0]);
+      if (ytpl.validateID(args)) {
+        const playlist = await ytpl(args);
         const videos = playlist.items;
         const count = videos.length;
         const embed = new Eris.RichEmbed()
           .setTitle(`**Added Playlist to Queue**`)
           .setDescription(
-            `✅ **${count} songs from [${playlist.title}](${args[0]})  has been added to the queue!**`
+            `✅ **${count} songs from [${playlist.title}](${args})  has been added to the queue!**`
           );
-        await bot.createMessage(message.channel.id, {
+        interactionPast.createMessage({
           embeds: [embed],
         });
         let song = {
@@ -60,7 +60,7 @@ async function music(bot, message, args, todo) {
         if (!database_queue) {
           database_queue = await new SQueue({
             _id: Types.ObjectId(),
-            guildID: message.guildID,
+            guildID: interactionPast.guildID,
             songs: [
               {
                 title: song.title,
@@ -73,17 +73,16 @@ async function music(bot, message, args, todo) {
         } else {
           await SQueue.findOneAndUpdate(
             {
-              guildID: message.guildID,
+              guildID: interactionPast.guildID,
             },
             {
               $push: {
-                songs: [
-                  {
-                    title: song.title,
-                    url: song.url,
-                    duration: song.duration,
-                  },
-                ],
+                songs:
+                {
+                  title: song.title,
+                  url: song.url,
+                  duration: song.duration,
+                },
               },
             }
           ); //note fix play
@@ -97,29 +96,28 @@ async function music(bot, message, args, todo) {
           };
           await SQueue.findOneAndUpdate(
             {
-              guildID: message.guildID,
+              guildID: interactionPast.guildID,
             },
             {
               $push: {
-                songs: [
-                  {
-                    title: song.title,
-                    url: song.url,
-                    duration: song.duration,
-                  },
-                ],
+                songs:
+                {
+                  title: song.title,
+                  url: song.url,
+                  duration: song.duration,
+                },
               },
             }
           );
         }
-        if (connectionMap.get(message.guildID)) {
+        if (connectionMap.get(interactionPast.guildID)) {
           return;
         } else {
-          return video_player(bot, message.guildID, message);
+          return video_player(bot, interactionPast.guildID, interactionPast);
         }
       } //note fix pla
-      if (ytdl.validateURL(args[0])) {
-        const song_info = await ytdl.getInfo(args[0]);
+      if (ytdl.validateURL(args)) {
+        const song_info = await ytdl.getInfo(args);
         const equation = (seconds) => {
           var detik = seconds % 60;
           var menit = Math.round(seconds / 60);
@@ -131,11 +129,18 @@ async function music(bot, message, args, todo) {
           url: song_info.videoDetails.video_url,
           duration: equation(song_info.videoDetails.lengthSeconds),
         };
-        return QueueUpdate(song, message, bot);
+        const embed = new Eris.RichEmbed()
+          .setTitle("**Added to Queue**")
+          .setDescription(`[${song.title}](${song.url})`);
+        interactionPast.createMessage({
+          embeds: [embed],
+        });
+        return QueueUpdate(song, interactionPast, bot);
       } else {
+        interactionPast.createMessage("Searching for **" + args + "**")
         let listsongs = "";
         let row = [];
-        const video_finder = async (query) => {
+        const video_finder = async (query, interactionPast) => {
           return new Promise(async function (resolve, reject) {
             //try and catch error
             const videoResult = await ytSearch(query);
@@ -158,7 +163,7 @@ async function music(bot, message, args, todo) {
               .setTitle(`**Choose song below by pressing the button**`)
               .setDescription(`${listsongs}`)
               .setFooter("To 'cancel' type cancel");
-            var m = await bot.createMessage(message.channel.id, {
+            var m = await interactionPast.createMessage({
               embeds: [embed1],
               components: [
                 {
@@ -172,10 +177,10 @@ async function music(bot, message, args, todo) {
             bot.on("interactionCreate", (interaction) => {
               if (interaction instanceof Eris.ComponentInteraction) {
                 if (interaction.message.id == m.id) {
-                  if (interaction.member.id == message.member.id) {
+                  if (interaction.member.id == interactionPast.member.id) {
                     m.delete();
                     return bot
-                      .createMessage(message.channel.id, {
+                      .createMessage(interactionPast.channel.id, {
                         embeds: [
                           {
                             title: "**Selected**",
@@ -200,7 +205,9 @@ async function music(bot, message, args, todo) {
               }
             });
             bot.on("messageCreate", (msg) => {
-              if (msg.member.id == message.member.id && !msg.bot) {
+              console.log(interactionPast.member.id)
+              console.log(msg.member.id)
+              if (msg.member.id == interactionPast.member.id && !msg.bot) {
                 if (msg.content == "cancel")
                   try {
                     return m.edit({
@@ -216,7 +223,7 @@ async function music(bot, message, args, todo) {
           });
         };
 
-        video_finder(args.join(" "));
+        video_finder(args, interactionPast);
 
         function video(video) {
           if (video) {
@@ -226,59 +233,37 @@ async function music(bot, message, args, todo) {
               duration: video.duration.timestamp,
             };
           }
-          return void QueueUpdate(song, message, bot);
+          return void QueueUpdate(song, interactionPast, bot);
         }
       }
       break;
     case "skip":
-      if (!connectionMap.get(message.guildID)) {
-        return bot.createMessage(
-          message.channel.id,
+      if (!connectionMap.get(interactionPast.guildID)) {
+        return interactionPast.createMessage(
           "There is no song currently playing"
         );
       }
 
-      if (
-        message.member.voiceState.channelID !=
-        connectionMap.get(message.guildID).channelID
-      ) {
-        return bot.createMessage(
-          message.channel.id,
-          "Please join the current voice channel"
-        );
-      }
-
-      connectionMap.get(message.guildID).stopPlaying();
-      bot.createMessage(
-        message.channel.id,
+      connectionMap.get(interactionPast.guildID).stopPlaying();
+      interactionPast.createMessage(
         `Skipping ${database_queue.songs[0].title}`
       );
       return;
     case "stop":
-      if (!connectionMap.get(message.guildID)) {
+      console.log(connectionMap.get(interactionPast.guildID))
+      if (!connectionMap.get(interactionPast.guildID)) {
         return bot.createMessage(
-          message.channel.id,
+          interactionPast.channel.id,
           "There is no song currently playing"
         );
       }
 
-      if (
-        message.member.voiceState.channelID !=
-        connectionMap.get(message.guildID).channelID
-      ) {
-        return bot.createMessage(
-          message.channel.id,
-          "Please join the current voice channel"
-        );
-      }
-
-      bot.createMessage(
-        message.channel.id,
+      interactionPast.createMessage(
         `Stopping the queue, deleting whole queue`
       );
       await SQueue.findOneAndUpdate(
         {
-          guildID: message.guildID,
+          guildID: interactionPast.guildID,
         },
         {
           $pullAll: {
@@ -286,20 +271,20 @@ async function music(bot, message, args, todo) {
           },
         }
       );
-      connectionMap.get(message.guildID).stopPlaying();
-      connectionMap.delete(message.guildID);
+      connectionMap.get(interactionPast.guildID).stopPlaying();
+      connectionMap.delete(interactionPast.guildID);
       return;
   }
 }
 
-async function QueueUpdate(song, message, bot) {
+async function QueueUpdate(song, interactionPast, bot) {
   let database_queue = await SQueue.findOne({
-    guildID: message.guildID,
+    guildID: interactionPast.guildID,
   });
   if (!database_queue) {
     database_queue = await new SQueue({
       _id: Types.ObjectId(),
-      guildID: message.guildID,
+      guildID: interactionPast.guildID,
       songs: [
         {
           title: song.title,
@@ -312,36 +297,35 @@ async function QueueUpdate(song, message, bot) {
   } else {
     await SQueue.findOneAndUpdate(
       {
-        guildID: message.guildID,
+        guildID: interactionPast.guildID,
       },
       {
         $push: {
-          songs: [
-            {
-              title: song.title,
-              url: song.url,
-              duration: song.duration,
-            },
-          ],
+          songs:
+          {
+            title: song.title,
+            url: song.url,
+            duration: song.duration,
+          },
         },
       }
     );
   }
   //note fix play
-  if (connectionMap.get(message.guildID) && database_queue.songs.length > 0) {
+  if (connectionMap.get(interactionPast.guildID) && database_queue.songs.length > 0) {
     const embed = new Eris.RichEmbed()
       .setTitle("**Added to Queue**")
       .setDescription(`[${song.title}](${song.url})`);
-    return bot.createMessage(message.channel.id, {
+    return interactionPast.createMessage({
       embeds: [embed],
     });
   } else {
     try {
-      return await video_player(bot, message.guildID, message);
+      return await video_player(bot, interactionPast.guildID, interactionPast);
     } catch (err) {
       await SQueue.findOneAndUpdate(
         {
-          guildID: message.guildID,
+          guildID: interactionPast.guildID,
         },
         {
           $pullAll: {
@@ -351,17 +335,17 @@ async function QueueUpdate(song, message, bot) {
       );
       console.log(err);
       bot.createMessage(
-        message.channel.id,
+        interactionPast.channel.id,
         "There was an error connecting to the voice channel"
       );
-      connectionMap.get(message.guildID).stopPlaying();
-      returnconnectionMap.delete(message.guildID);
+      connectionMap.get(interactionPast.guildID).stopPlaying();
+      // return connectionMap.delete(interactionPast.guildID);
       throw err;
     }
   }
 }
 
-async function video_player(bot, guild, message) {
+async function video_player(bot, guild, interactionPast) {
   const database_queue = await SQueue.findOne({
     guildID: guild,
   });
@@ -369,90 +353,106 @@ async function video_player(bot, guild, message) {
   console.log(database_queue.songs.length);
   if (database_queue.songs.length == 0) {
     bot.createMessage(
-      message.channel.id,
+      interactionPast.channel.id,
       `Leaving voice channel due to inactivity`
     );
-    bot.leaveVoiceChannel(message.member.voiceState.channelID);
-    connectionMap.delete(message.guildID);
-    return connectionMap.delete(message.guildID);
+    bot.leaveVoiceChannel(interactionPast.member.voiceState.channelID);
+    connectionMap.delete(interactionPast.guildID);
+    return connectionMap.delete(interactionPast.guildID);
   }
   bot
-    .joinVoiceChannel(message.member.voiceState.channelID)
+    .joinVoiceChannel(interactionPast.member.voiceState.channelID)
     .catch((err) => {
       // Join the user's voice channel
       bot.createMessage(
-        message.channel.id,
+        interactionPast.channel.id,
         "Error joining voice channel: " + err.message
       ); // Notify the user if there is an error
       console.log(err); // Log the error
     })
-    .then((connection) => {
-      const download = ytdl(database_queue.songs[0].url, {
+    .then(async (connection) => {
+      const path = '../../songs/' + interactionPast.guildID + '/' + database_queue.songs[0].title
+      const result = ytdl(database_queue.songs[0].url, {
         filter: "audioonly",
-        quality: "highestaudio",
-      });
-      connection.play(download);
-      const embedplaying = new Eris.RichEmbed()
-        .setTitle("**Now Playing**")
-        .setDescription(
-          `[${database_queue.songs[0].title}](${database_queue.songs[0].url}) \n` +
-          "`[00:00/" +
-          database_queue.songs[0].duration +
-          "]`"
-        );
-      connectionMap.set(message.guildID, connection);
-      console.log("playing");
-      bot.createMessage(message.channel.id, {
-        embeds: [embedplaying],
-      });
-      connection.once("end", async () => {
-        console.log("ending")
-        const queueShift = async (guild) => {
-          let database_queue = await SQueue.findOne({
-            guildID: guild,
-          });
-          if (database_queue.songs.length == 1) {
-            await SQueue.findOneAndUpdate(
-              {
-                guildID: guild,
-              },
-              {
-                $pullAll: {
-                  songs: database_queue.songs,
-                },
-              }
-            );
-            return await video_player(bot, guild, message);
-          } else {
-            await SQueue.findOneAndUpdate(
-              {
-                guildID: guild,
-              },
-              {
-                $pop: {
-                  songs: -1,
-                },
-              }
-            );
-            return await video_player(bot, guild, message);
-          }
-        };
-        await queueShift(guild);
-      });
-      connection.on("err", err => {
-        bot.createMessage(message.channel.id, "Error innitiating" + err);
-        SQueue.findOneAndUpdate(
-          {
-            guildID: guild,
-          },
-          {
-            $pop: {
-              songs: -1,
-            },
-          }
-        );
-        return video_player(bot, guild, message);
+        quality: "lowestaudio",
+        format: "webm"
       })
+      // .pipe(fs.createWriteStream(path)).on('finish', (output) => {
+      //   if (output) {
+      //     resolve(path)
+      //   }
+      // })
+      setTimeout(() => {
+        connection.play(result)
+
+        const embedplaying = new Eris.RichEmbed()
+          .setTitle("**Now Playing**")
+          .setDescription(
+            `[${database_queue.songs[0].title}](${database_queue.songs[0].url}) \n` +
+            "`[00:00/" +
+            database_queue.songs[0].duration +
+            "]`"
+          );
+        connectionMap.set(interactionPast.guildID, connection);
+        console.log("playing");
+        bot.createMessage(interactionPast.channel.id, {
+          embeds: [embedplaying],
+        });
+        connection.once("end", async () => {
+          console.log("ending")
+          const queueShift = async (guild) => {
+            let database_queue = await SQueue.findOne({
+              guildID: guild,
+            });
+            if (database_queue.songs.length == 1) {
+              await SQueue.findOneAndUpdate(
+                {
+                  guildID: guild,
+                },
+                {
+                  $pullAll: {
+                    songs: database_queue.songs,
+                  },
+                }
+              );
+              return await video_player(bot, guild, interactionPast);
+            } else {
+              await SQueue.findOneAndUpdate(
+                {
+                  guildID: guild,
+                },
+                {
+                  $pop: {
+                    songs: -1,
+                  },
+                }
+              );
+              return await video_player(bot, guild, interactionPast);
+            }
+          };
+          await queueShift(guild);
+        });
+        connection.on("err", err => {
+          bot.createMessage(interactionPast.channel.id, "Error innitiating" + err);
+          SQueue.findOneAndUpdate(
+            {
+              guildID: guild,
+            },
+            {
+              $pop: {
+                songs: -1,
+              },
+            }
+          );
+          return video_player(bot, guild, interactionPast);
+        })
+      }, 7500)
+      // await connection.play(ytdl(database_queue.songs[0].url, {
+      //   filter: "audioonly",
+      //   quality: "lowestaudio",
+      // }), {
+      //   voiceDataTimeout: 1000
+      // })
     });
 }
 
